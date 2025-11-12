ee72e9dc4e914d5729703ea3530c3ec6c3bf6406
dion-zebua <zebuadbless@gmail.com>
2025-11-12 11:55:29 +0700

    malas ngoding, paham 🖐

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,19 @@
 
 <head>
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-9KM5SST404"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-9KM5SST404');
+    </script>
+
     @php
         $resultTitle = ($title ?? web()->title) . ' | ' . web()->title;
         $resultDescription = ($description ?? web()->tagline) . ' | ' . web()->title;
